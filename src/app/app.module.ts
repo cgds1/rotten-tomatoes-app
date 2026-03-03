02e4d6a39dd35253fe00e6bca5c1c2f920410c84
Carlos Diaz <cg22ds1243@gmail.com>
2026-03-03 00:25:06 -0400

    feat: scaffold Ionic + Angular project with Capacitor
    Set up the base Ionic 8 + Angular 20 project with Capacitor for
    Android deployment. Includes routing, environment configs, editor
    settings, and updated .gitignore for Ionic/Angular workflow.

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { environment } from '../environments/environment';
+import { AUTH_SERVICE, MOVIES_SERVICE, COMMENTS_SERVICE } from './core/services/service-tokens';
+import { AuthMockService } from './core/services/auth.mock.service';
+import { MoviesMockService } from './core/services/movies.mock.service';
+import { CommentsMockService } from './core/services/comments.mock.service';
+
+const serviceProviders = environment.useMocks
+  ? [
+      { provide: AUTH_SERVICE, useClass: AuthMockService },
+      { provide: MOVIES_SERVICE, useClass: MoviesMockService },
+      { provide: COMMENTS_SERVICE, useClass: CommentsMockService },
+    ]
+  : [
+      // TODO: HTTP service implementations for production
+      // { provide: AUTH_SERVICE, useClass: AuthHttpService },
+      // { provide: MOVIES_SERVICE, useClass: MoviesHttpService },
+      // { provide: COMMENTS_SERVICE, useClass: CommentsHttpService },
+    ];
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    ...serviceProviders,
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
